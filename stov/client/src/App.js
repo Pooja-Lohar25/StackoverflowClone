0d066067860da9fe0d6d200be2a972cf7d507afa
pooja <poojalohar789@gmail.com>
2023-09-25 22:27:50 +0530

    add authentication page

--- stov/client/src/App.js
+++ stov/client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import AllRoutes from './components/Routes/AllRoutes';
 
+
+
 function App() {
   return (
     <div className="App">
--- stov/client/src/components/Auth/Auth.jsx
+++ stov/client/src/components/Auth/Auth.jsx
@@ -1,11 +1,18 @@
 import React,{useState} from 'react'
-import logo from '../../assets/stack-overflow.svg'
+import logo from '../../assets/stack-overflow-icon.png'
+import AboutAuth from './AboutAuth'
+import "./Auth.css"
+
+
 const Auth = () => {
     const [isSignup, setIsSignup] = useState(false)
   return (
     <section className='auth-section'>
-        <div className='auth-container'>
-            { !isSignup && <img src={logo} alt='stack overflow' height='200px' className='login-logo' /> }
+        {
+            isSignup && <AboutAuth/>
+        }
+        <div className='auth-container-2'>
+            { !isSignup && <img src={logo} alt='stack overflow' height='50px' className='login-logo' /> }
             <form>
                 {
                     isSignup && (
@@ -20,13 +27,38 @@ const Auth = () => {
                     <input type='email' name='email'></input>
                 </label>
                 <label htmlFor='password'>
-                    <div>
+                    <div style={{display : "flex", justifyContent : 'space-between'}}>
                         <h4>Password</h4>
-                        {!isSignup && <h4>Forgot Password?</h4>}
+                        {!isSignup && <p style={{fontSize:"13px", color:"#007ac6"}}>Forgot Password?</p>}
                     </div>
                     <input type='password' name='password' id='password'></input>
+                    {isSignup && 
+                        <p style={{color:"#666767",fontSize:"13px"}}>
+                        Password must contain at least eight <br/> 
+                        characters, including at least 1 letter and 1 <br/> 
+                        number</p>}
                 </label>
+                {
+                    isSignup && (
+                        <label htmlFor='check'>
+                            <input type='checkbox' id='check'></input>
+                            <p>Opt-in to recieve occasional, <br/> 
+                            product updates, user research invitations, <br/> 
+                            copany announcements and digests. </p>
+                        </label>
+                    )
+                }
                 <button type='submit' className='auth-btn'  >{ isSignup ? 'Signup' : 'Login'}</button>
+                {
+                    isSignup && (
+                        <p style={{color:"#666767",fontSize:"13px"}}>
+                            By clicking signup, you agree to our <br/>
+                            <span style={{color:"#007ac6"}}>terms of service</span>, 
+                            <span style={{color:"#007ac6"}}>privacy policy</span> and 
+                            <span style={{color:"#007ac6"}}>cookie policy</span>
+                        </p>
+                    )
+                }
             </form>
             <p>
                 {isSignup ? 'Already have an account' : "don't have an account?"}
