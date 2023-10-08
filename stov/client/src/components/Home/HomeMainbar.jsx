d9b37e8416f9ffbac41e3e36e45838b2d420c7a5
pooja <poojalohar789@gmail.com>
2023-10-08 22:16:12 +0530

    add home-mainbar

--- stov/client/src/components/Home/HomeMainbar.jsx
+++ stov/client/src/components/Home/HomeMainbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import '../../App.css'
+import {Link, useLocation} from 'react-router-dom'
 
 const HomeMainbar = () => {
+  const location = useLocation();
+
   return (
-    <div style={{border:"solid"}}>
-      
+    <div className='home-mainbar'>
+      <div>
+        {
+          location.pathname === '/' ? <p>Top Questions</p> : <p>All Questions</p>
+        }
+        
+        <button>Ask Question</button>
+      </div>
+      <p>8 Questions</p>
+      <div className='questions-container'>
+        <div className='question'>
+        </div>
+      </div>
     </div>
   )
 }
